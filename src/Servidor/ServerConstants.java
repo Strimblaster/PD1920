215561699e0fa600fee52ebc2f21cffb19583fb2
Emanuel Ferreira <emanuels.ferreira@hotmail.com>
2019-12-03 19:40:29 +0000

    Constantes em Interfaces E Simplificação do Server

---
+++ src/Servidor/ServerConstants.java
@@ -0,0 +1,8 @@
+package Servidor;
+
+public interface ServerConstants {
+    String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    String DB_URL = "jdbc:mysql://34.77.114.162/?autoReconnect=true&useSSL=false";
+    String USER = "PD";
+    String PASS = "PDcancro";
+}
