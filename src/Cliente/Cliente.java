74fd7825da295a06aef0f1899beeb55bf95ae783
André Filipe Melo <melo.andre.filipe@gmail.com>
2019-12-03 19:00:11 +0000

    Login JFX

--- src/Cliente/Cliente.java
+++ src/Cliente/Cliente.java
@@ -1,10 +1,28 @@
 package Cliente;
 
-import java.net.Socket;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Cliente {
+public class Cliente extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("javaFX/login.fxml"));
+            BorderPane root =  loader.load();
+            Scene scene = new Scene(root);
+
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception e)    {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 }
--- src/Cliente/Comunicacao.java
+++ src/Cliente/Comunicacao.java
@@ -5,7 +5,6 @@ import java.net.Socket;
 
 public class Comunicacao implements IComunicacao {
     Socket s;
-
     public Comunicacao(Socket s) {
         this.s = s;
     }
