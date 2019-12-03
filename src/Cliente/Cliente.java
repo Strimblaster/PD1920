f7788e600de8608af10ca67c063e9828f4ea3d8f
André Filipe Melo <melo.andre.filipe@gmail.com>
2019-12-03 20:25:59 +0000

    Comunicação Cliente -> DS
    	- Ainda falta passar o cliente a comunicar com o servidor;

--- src/Cliente/Cliente.java
+++ src/Cliente/Cliente.java
@@ -7,9 +7,16 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Cliente extends Application {
+    private ClientController clientController = new ClientController();
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
         try {
+            clientController.sendMessageDS();
             FXMLLoader loader = new FXMLLoader(getClass().getResource("javaFX/login.fxml"));
             BorderPane root =  loader.load();
             Scene scene = new Scene(root);
@@ -22,7 +29,4 @@ public class Cliente extends Application {
         }
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
--- src/DS/ServerThread.java
+++ src/DS/ServerThread.java
@@ -4,8 +4,6 @@ import Comum.ServerInfo;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
 
 public class ServerThread extends Thread {
 
