f5674dd1d48f3c926d4e1e8fc67ac2e350c3dab1
mickaelsantos <mickaelfsantos@gmail.com>
2019-12-23 20:03:14 +0000

    continua a busca das musicas

--- src/Cliente/javaFX/MainController.java
+++ src/Cliente/javaFX/MainController.java
@@ -1,12 +1,16 @@
 package Cliente.javaFX;
 
+import Cliente.ClientController;
+import com.sun.tools.javac.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.stage.FileChooser;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,6 +21,9 @@ public class MainController extends SceneController implements Initializable {
     public Button btnUpload;
     public Button btnSearch;
     public Button btnVoltar;
+    public VBox playlists;
+    public VBox muscias;
+
 
     public void handleBtnUpload(ActionEvent actionEvent) {
         try {
@@ -29,6 +36,14 @@ public class MainController extends SceneController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+
+    @Override
+    public void setClientController(ClientController sceneController) {
+        super.setClientController(sceneController);
+        playlists.getChildren().clear();
+        muscias.getChildren().clear();
 
     }
 }
